33611314f5efd022486c70fbe4f0b30f97477ea7
IgorMeloMoraes <igor-batista@outlook.com.br>
2024-01-13 17:52:27 -0300

    Versão 1.16.1 - Estudo Audio na tela de splash e/ou Loading

--- app/src/main/java/com/example/meusestudosandroid/LoadingActivity_17.java
+++ app/src/main/java/com/example/meusestudosandroid/LoadingActivity_17.java
@@ -3,15 +3,22 @@ package com.example.meusestudosandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class LoadingActivity_17 extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_17);
+
+        // Audio
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio_1);
+        mediaPlayer.start();
 
         // A loading Activity é muito parecido com a splash screen, porem pode ser utilizada para outros carregamentos e em outros locais
         // Para isso, utilizamos a mesma estrutura da splashScreen, porem adicionamos um botão de loading no xml, esse loading carrega
@@ -21,6 +28,10 @@ public class LoadingActivity_17 extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(LoadingActivity_17.this, MainActivity.class));
+
+                // Podemos adicionar um audio para uma tela especifica e para-lo quando ir para uma outro tela
+                // Pode ser um audio de introdução ou de carregamento de tela
+                mediaPlayer.stop();
                 finish();
             }
         },3000);
